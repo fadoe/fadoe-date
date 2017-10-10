960f93924c8a4f91855f92b2ac187c60c110748a
Falk Doering <falk.doering@fadoe.de>
2017-10-10 18:37:11 +0200

    update date time zone to php 7

--- src/DateTimeZone.php
+++ src/DateTimeZone.php
@@ -1,15 +1,14 @@
 <?php
+
 namespace FaDoe\Date;
 
 class DateTimeZone extends \DateTimeZone
 {
-
     /**
      * @return string
      */
-    public function __toString()
+    public function __toString(): string
     {
         return $this->getName();
     }
-
 }
